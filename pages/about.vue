b41dabd4fa50946400672529a247ab6d8df9daf9
mastinoz <persiste1@gmail.com>
2018-06-03 21:31:39 +0000

    just a test

---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>THE ABOUT PAGE</h1>
+</template>
---
+++ pages/recipes.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="recipes">
+        <Recipe
+        thumbnail="https://static.pexels.com/photos/2232/vegetables-italian-pizza-restaurant.jpg"
+        title="Delicious Pizza"
+        previewText="Awesome Pizza!" />
+        <Recipe
+        thumbnail="https://static.pexels.com/photos/37405/food-prawn-asian.jpg"
+        title="Sea Food Salad"
+        previewText="Very tasty indeed!" />
+    </section>
+</template>
+<script>
+import Recipe from "@/components/Recipe"
+export default {
+  components: {
+    Recipe
+  }
+}
+</script>
+<style scoped>
+.recipes{
+display: flex;
+flex-flow: row wrap;
+justify-content: center;
+align-items: center;
+}
+</style>
+
